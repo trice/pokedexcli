2be74509f0936e3fc09c7da7f570630182fa2fee
Terry Rice <trice7454@gmail.com>
2025-07-15 14:28:52 -0500

    Some basic tests added

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
+func cleanInput(text string) []string {
+	if len(text) == 0 {
+		return []string{}
+	}
+
+	words := strings.Fields(text)
+
+	for i := range words {
+		words[i] = strings.ToLower(words[i])
+	}
+
+	return words
+}
+
 func main()  {
-	fmt.Println("Hello, World!")
+	for _, v := range cleanInput(" HELLO     World ") {
+		fmt.Println(v)
+	}
 }
---
+++ repl_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T)  {
+	cases := []struct {
+		input string
+		expected []string
+	}{
+		{
+			input: " hello world ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: "",
+			expected: []string{},
+		},
+	}
+
+	for _, testCase := range cases {
+		actual := cleanInput(testCase.input)
+		
+		if len(actual) != len(testCase.expected) {
+			t.Errorf("lengths of actual vs expected did not match")
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := testCase.expected[i]
+			if word != expectedWord {
+				t.Errorf("word: %s did not match expectedWord: %s", word, expectedWord)
+			}
+		}
+	}
+}
